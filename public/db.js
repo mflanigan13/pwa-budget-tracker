176faa1276c2bf40025982a13c2922600376abf5
mflanigan <mflanigantwualumn@gmail.com>
2022-03-26 13:40:41 -0500

    initial file setup

---
+++ public/db.js
@@ -0,0 +1,15 @@
+// TODO: open  indexedDB
+const indexedDB =
+window.indexedDB ||
+window.mozIndexedDB ||
+window.webkitIndexedDB ||
+window.msIndexedDB ||
+window.shimIndexedDB;
+
+let db;
+const request = indexedDB.open("budget", 1);
+
+
+function saveRecord(record) {
+  // add more code later
+}
---
+++ public/service-worker.js
@@ -0,0 +1,4 @@
+// TODO: implement service worker so that users can use the app offline. The SW
+// will need to cache static assets to display the app offline. Additionally,
+// the SW should cache transaction data, using the cached data as a fallback
+// when the app is used offline. 
